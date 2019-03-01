06e3e763c28f4b2822798da60bff5e6b6eb6ab62
Michael Bestas <mkbestas@lineageos.org>
2019-03-01 23:52:02 +0200

    drivers: Import FIH drivers
    * Package version: O-MR2-OPM1-RZR-180817.6033.tar.gz
    * Estimated base tag: LA.UM.6.4.r1-06500-8x98.0
    * Rebase tag: LA.UM.6.4.r1-11500-8x98.0
    
    Change-Id: I898070485af7d6a4d7f544655b0f7559b44dc4f3

---
+++ include/fih/fih_rere.h
@@ -0,0 +1,10 @@
+#ifndef __FIH_RERE_H
+#define __FIH_RERE_H
+
+#include <dt-bindings/msm/power-on.h>
+
+#define FIH_RERE_ANDROID_MODE         0x00
+#define FIH_RERE_REBOOT_DEVICE        0x2B
+
+#endif
+
