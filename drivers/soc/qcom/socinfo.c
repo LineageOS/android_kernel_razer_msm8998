c40b209a1333f454b93099d593dddaefbe2da988
Michael Bestas <mkbestas@lineageos.org>
2019-03-05 00:56:35 +0200

    soc: qcom: socinfo: Setup FIH HWID
    * Package version: O-MR2-OPM1-RZR-180817.6033.tar.gz
    * Estimated base tag: LA.UM.6.4.r1-06500-8x98.0
    * Rebase tag: LA.UM.6.4.r1-11500-8x98.0
    
    Change-Id: I408a05739bd64786d7df5b39d57b9102934f7b58

--- drivers/soc/qcom/socinfo.c
+++ drivers/soc/qcom/socinfo.c
@@ -34,6 +34,9 @@
 #include <soc/qcom/socinfo.h>
 #include <soc/qcom/smem.h>
 #include <soc/qcom/boot_stats.h>
+#ifdef CONFIG_FIH_9801
+#include <fih/hwid.h>
+#endif
 
 #define BUILD_ID_LENGTH 32
 #define SMEM_IMAGE_VERSION_BLOCKS_COUNT 32
@@ -1601,6 +1604,11 @@ int __init socinfo_init(void)
 	socinfo_print();
 	arch_read_hardware_id = msm_read_hardware_id;
 	socinfo_init_done = true;
+
+#ifdef CONFIG_FIH_9801
+	/* FIH, initial hwid mechanism */
+	fih_hwid_setup();
+#endif
 
 	return 0;
 }
