03caad7de2d8fdadd804129e6ea4d932732446b8
Michael Bestas <mkbestas@lineageos.org>
2019-03-04 20:07:06 +0200

    msm: camera: Fix camera close takes more than 10 seconds
    * Package version: O-MR2-OPM1-RZR-180817.6033.tar.gz
    * Estimated base tag: LA.UM.6.4.r1-06500-8x98.0
    
    Change-Id: I8b54ec76bf8e4c51b120260bb9691d5259666331

--- drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
+++ drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
@@ -842,6 +842,19 @@ static int32_t msm_actuator_park_lens(struct msm_actuator_ctrl_t *a_ctrl)
 		a_ctrl->park_lens.max_step = a_ctrl->max_code_size;
 
 	next_lens_pos = a_ctrl->step_position_table[a_ctrl->curr_step_pos];
+
+#ifdef CONFIG_MACH_RCL
+	// DAC data size is 10 bits, value = 0~1024
+	if ((next_lens_pos < 0) || (next_lens_pos > 1024)) {
+		int new_lens_pos = 0;
+		if (next_lens_pos > 1024)
+			new_lens_pos = 1024;
+		pr_err("Cam_%d: Change next_lens_pos from %d to %d \n",
+			a_ctrl->cam_name, next_lens_pos, new_lens_pos);
+		next_lens_pos = new_lens_pos;
+	}
+#endif
+
 	while (next_lens_pos) {
 		/* conditions which help to reduce park lens time */
 		if (next_lens_pos > (a_ctrl->park_lens.max_step *
