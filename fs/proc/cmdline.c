cde4b48f5cc1212dbac5b76bf07bfd6452accf9c
Nolen Johnson <johnsonnolen@gmail.com>
2024-01-04 16:13:10 -0500

    Revert "proc: Remove console flag from /proc/cmdline"
    This reverts commit ec199d6fd8ed93fed7afda84d2b2b6f1c534b690.
    
    Reason: Conflicts with 2si, needs rework.
    Change-Id: I39e37013ca168edf89ce0876071effff136a8bec

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -2,19 +2,10 @@
 #include <linux/init.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
-#ifdef CONFIG_MACH_RCL
-#include <asm/setup.h>
-
-static char new_command_line[COMMAND_LINE_SIZE];
-#endif
 
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
-#ifdef CONFIG_MACH_RCL
-	seq_printf(m, "%s\n", new_command_line);
-#else
 	seq_printf(m, "%s\n", saved_command_line);
-#endif
 	return 0;
 }
 
@@ -32,31 +23,6 @@ static const struct file_operations cmdline_proc_fops = {
 
 static int __init proc_cmdline_init(void)
 {
-#ifdef CONFIG_MACH_RCL
-	char *offset_addr, *cmd = new_command_line;
-
-	strcpy(cmd, saved_command_line);
-
-	/*
-	 * Remove 'androidboot.console' flag from command line seen
-	 * by userspace in order to stop console service from crashing.
-	 */
-	offset_addr = strstr(cmd, "androidboot.console=");
-	if (offset_addr) {
-		size_t i, len, offset;
-
-		len = strlen(cmd);
-		offset = offset_addr - cmd;
-
-		for (i = 1; i < (len - offset); i++) {
-			if (cmd[offset + i] == ' ')
-				break;
-		}
-
-		memmove(offset_addr, &cmd[offset + i + 1], len - i - offset);
-	}
-#endif
-
 	proc_create("cmdline", 0, NULL, &cmdline_proc_fops);
 	return 0;
 }
